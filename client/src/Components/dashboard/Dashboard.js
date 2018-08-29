3c095c227689dc7cfc4f3054c003cf631236a0a7
Rakesh <rakesh@www.supporttrai.host>
2018-08-29 17:48:02 +0530

    profile create form

--- client/src/Components/dashboard/Dashboard.js
+++ client/src/Components/dashboard/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 import {connect } from 'react-redux';
 import {getCurrentProfile} from '../../actions/profileActions';
+import Spinner from '../common/spinner';
+import Link from 'react-router-dom/Link';
 
 class Dashboard extends Component {
 
@@ -10,9 +12,39 @@ class Dashboard extends Component {
     }
     
   render() {
-    return (
-      <div>
+    const {user} = this.props.auth;
+    const {profile, loading} = this.props.profile;
+
+    let dashboard;
+
+    if(profile == null || loading){
+        dashboard = <Spinner/>
+    }else {
+        // check if user has profile
+        if(Object.keys(profile).length > 0){
+            dashboard = <h2>TODO : Profile</h2>
+        }else {
+            dashboard = (
+                <div>
+                    <p>You have not created a profile yet</p>
+                    <Link to="/create-profile" className="btn btn-lg btn-info">Create Profile</Link>
+                </div>
+            );
+        }
         
+    }
+
+
+    return (
+      <div className="dashboard">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4">Dashboard</h1>
+                    {dashboard}
+                </div>
+            </div>
+        </div>   
       </div>
     )
   }
@@ -20,13 +52,13 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
     profile : PropTypes.object.isRequired,
-    loading : PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
     getCurrentProfile : PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
     profile : state.profile,
-    loading : state.loading
+    auth: state.auth
 })
 
 export default connect(mapStateToProps,{getCurrentProfile})(Dashboard);
