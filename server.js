1cf0ead14440ca5bf770a0e03fc74b32e5c920d2
Rakesh Jha <rakesh.badlaaw@gmail.com>
2019-04-25 14:25:27 -0700

    added cors

--- server.js
+++ server.js
@@ -1,39 +1,44 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const db = require('./config/keys').mongoURI;
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const db = require("./config/keys").mongoURI;
+const cors = require("cors");
 
 var app = express();
 var port = process.env.PORT || 5000;
 
-var users = require('./routes/apis/users');
-var posts = require('./routes/apis/post');
-var profiles = require('./routes/apis/profile');
+var users = require("./routes/apis/users");
+var posts = require("./routes/apis/post");
+var profiles = require("./routes/apis/profile");
 
-app.get('/', (req, res) => {
-    res.send('I am Okay');
+app.get("/", (req, res) => {
+  res.send("I am Okay");
 });
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(bodyParser.json());
 app.use(passport.initialize());
-
+app.use(cors());
 // Passport Config
-require('./config/passport.js')(passport);
+require("./config/passport.js")(passport);
 
+mongoose
+  .connect(db)
+  .then(() => console.log("mongodb connected"))
+  .catch(err => console.log(err.message));
 
-mongoose.connect(db).then(() => console.log('mongodb connected')).catch((err) => console.log(err.message));
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profiles", profiles);
 
-app.use('/api/users', users);
-app.use('/api/posts', posts);
-app.use('/api/profiles', profiles);
-
-app.listen(port, (err) => {
-    if (err) {
-        console.log(`Error : ${err.message}`);
-    }
-    console.log(`app is listening on ${port}`);
+app.listen(port, err => {
+  if (err) {
+    console.log(`Error : ${err.message}`);
+  }
+  console.log(`app is listening on ${port}`);
 });
